a19cb0b42563ac195b96c8bdd50296bb6bc61edc
Martin Lupa <martinignaciolupa@gmail.com>
2023-09-04 09:01:59 +0200

    remove auth0 route from app router

---
+++ protected-resource-server/dist/src/index.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const data_json_1 = __importDefault(require("../secret-data/data.json"));
+const cors = require("cors");
+const app = (0, express_1.default)();
+const port = 5000;
+app.use(cors());
+app.use(express_1.default.json());
+app.get("/", (req, res) => {
+    res.json(data_json_1.default);
+});
+app.listen(port, () => {
+    console.log(`Server listening on port: ${port}`);
+});
